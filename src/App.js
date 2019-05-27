1cf90b2fab537401aa5f611d0196dc7a74a009d6
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2019-05-27 17:22:42 -0300

    Fetch dynamic content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('Loading...')
+  const [description, setDescription] = useState('Hold on while we load this content.')
+
+  useEffect(() => {
+    fetch('https://swapi.co/api/people/1/?format=json')
+      .then(data => data.json())
+      .then(json => {
+        const { name, birth_year } = json
+        setTitle(name)
+        setDescription(`This iconic character was born in ${birth_year}.`)
+      })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Helmet>
+      
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
